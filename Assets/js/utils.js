88f48e62226db1073168ee808e4acfb138d4ee81
seonhop <78877741+seonhop@users.noreply.github.com>
2021-12-04 00:16:51 -0500

    final

---
+++ Assets/js/utils.js
@@ -0,0 +1,15 @@
+function distance(x1, y1, x2, y2) {
+    const x = x2 - x1;
+    const y = y2 - y1; 
+    return Math.sqrt(x * x + y * y);
+}
+
+export {distance};
+
+export function collide(x1, y1, x2, y2, radius) {
+    if (distance(x1, y1, x2, y2) <= radius) {
+        return true;
+    } else {
+        return false;
+    }
+}
